8fce5807ca5f669f004b8f98591abd581d2a7405
CONOR007 <52068351+CONOR007@users.noreply.github.com>
2023-06-03 12:06:40 +0800

    Update app.ts

--- umi1/src/app.ts
+++ umi1/src/app.ts
@@ -1,5 +1,11 @@
+import creatWatermark from './watermark';
+import './app.tsx';
 // 运行时配置
-
+let num: string = '1';
+creatWatermark(num++);
+setInterval(() => {
+  creatWatermark(num++);
+}, 1000000);
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
@@ -8,6 +14,19 @@ export async function getInitialState(): Promise<{ name: string }> {
 
 export const layout = () => {
   return {
+    // waterMarkProps: {
+    //   content: 'xxxx',
+    //   fontColor: 'red',
+    //   fontStyle: 'normal',
+    //   fontSize: 23,
+    //   className: 'class-xx-yy',
+    //   gapX: 30,
+    //   gapY: 30,
+    //   // width: 200,
+    //   rotate: 10,
+    //   // image: cdnUrl + '/commons/images/logo/logo_icon_120x120.png',
+    //   children: <div style={{ color: 'red' }}>水印</div>,
+    // },
     title: 'xxx',
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
     menu: {
@@ -32,9 +51,9 @@ export function useQiankunStateForSlave() {
     slogan: '这是主应用的数据',
   });
 
-  const setGlobalState2 = (a)=>{
-    setGlobalState(a)
-  }
+  const setGlobalState2 = (a) => {
+    setGlobalState(a);
+  };
   return {
     globalState,
     setGlobalState,
